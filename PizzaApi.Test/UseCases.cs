8b15b5e3f2473b6c80fb2d2013d985190072214b
Ricardo Brito Brens <Ricardo_Brito@bancocaribe.com.do>
2022-02-02 16:39:00 -0400

    [ADD] test project

---
+++ PizzaApi.Test/UseCases.cs
@@ -0,0 +1,23 @@
+using NUnit.Framework;
+
+namespace PizzaApi.Test
+{
+    [TestFixture]
+    public class UseCases
+    {
+        [SetUp]
+        public void SetUp()
+        {
+        }
+
+        [Test]
+        public void Test()
+        {
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+        }
+    }
+}
