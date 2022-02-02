f5cffc6ef7f076143b24c76fcbe4915e24e7c03d
Ricardo Brito Brens <Ricardo_Brito@bancocaribe.com.do>
2022-02-02 16:40:33 -0400

    [REN] application dbcontext class

---
+++ PizzaApi/Persistence/PizzaContextDb.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PizzaApi.Core.Models;
+
+namespace PizzaApi.Persistence
+{
+    public class PizzaContextDb: DbContext
+    {
+        public DbSet<Pizza> Pizzas { get; set; }
+        public PizzaContextDb(DbContextOptions dbContextOptions): base(dbContextOptions)
+        {
+
+        }
+
+    }
+}
