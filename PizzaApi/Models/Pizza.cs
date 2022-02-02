50f1a648442c1da50c4383833b97009fabe146d4
Ricardo Brito Brens <Ricardo_Brito@bancocaribe.com.do>
2022-02-02 16:40:03 -0400

    [REM] pizza model to a better location

--- PizzaApi/Models/Pizza.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PizzaApi.Models
-{
-    public class Pizza
-    {
-        public int Id { get; set; }
-        public string? Name { get; set; }
-        public string? Description { get; set; }
-    };
-}
